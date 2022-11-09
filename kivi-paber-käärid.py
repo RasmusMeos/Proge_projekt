f9576f9b3899845eebb645a39e041ef58f9a858d
RasmusMeos <rasmus@meos.ee>
2022-11-09 21:09:49 +0200

    Update kivi-paber-käärid.py

--- kivi-paber-käärid.py
+++ kivi-paber-käärid.py
@@ -1,57 +1,115 @@
 import random
 
+võimalused = ["kivi", "paber","käärid"]
+mängude_arv = 5
+võite_vaja = 3
+
+mäs = 0 # mängija võite
+ars = 0 #arvuti võite
+
 while True:
-
     mängija_valik = input("Vali kas kivi, paber või käärid: ")
-    võimalused = ["kivi", "paber", "käärid"]
     arvuti_valik = random.choice(võimalused)
-
-    ars = 0 # arvuti skoor
-    mäs = 0 # mängija skoor
-
-    print("Sinu valik: "+mängija_valik)
-    print("Arvuti valik: "+arvuti_valik)
-    
 
     if mängija_valik == arvuti_valik:
         print("Tegid arvutiga sama valiku. Te jäite viiki!")
         print("Seis on",mäs,":",ars)
 
-    elif mängija_valik == "kivi":
-        if arvuti_valik == "käärid":
-            mäs += 1
-            print("Kivi lõhub käärid - sinu võit!")
-            print("Seis on",mäs,":",ars)
-        else: 
-            ars += 1
-            print("Paber katab kivi - arvuti võit!")
-            print("Seis on",mäs,":",ars)
+    elif mängija_valik == "kivi" and arvuti_valik == "paber":
+        ars += 1
+        print("Paber katab kivi - arvuti võit!")
+        print("Seis on",mäs,":",ars)
+    elif mängija_valik == "kivi" and arvuti_valik == "käärid":
+        mäs += 1
+        print("Kivi lõhub käärid - sinu võit!")
+        print("Seis on",mäs,":",ars)
+    
+    elif mängija_valik == "käärid" and arvuti_valik == "paber":
+        mäs += 1
+        print("Käärid lõikavad paberi - sinu võit!")
+        print("Seis on",mäs,":",ars)
+    elif mängija_valik == "käärid" and arvuti_valik == "kivi":
+        ars += 1
+        print("Kivi lõhub käärid - arvuti võit!")
+
+    elif mängija_valik == "paber" and arvuti_valik == "kivi":
+        mäs += 1
+        print("Paber katab kivi - sinu võit!")
+        print("Seis on",mäs,":",ars)
+    elif mängija_valik == "paber" and arvuti_valik == "käärid":
+        ars += 1
+        print("Käärid lõikavad paberi - arvuti võit!")
+        print("Seis on",mäs,":",ars)
+
+    if mäs == võite_vaja or ars == võite_vaja:
+        break
+
+if mäs > ars:
+    print("Õnnitlused, oled võitnud kolme võiduni mängitava seeria!")
+    print("Lõppseis jäi:",mäs,":",ars)
+else:
+    print("Proovi uuesti! Arvuti võitis selle kolme võiduni mängitava seeria.")
+
+
+
+
+
+    # import random
+
+# ars = 0 # arvuti skoor
+# mäs = 0 # mängija skoor
+# while True:
+
+#     mängija_valik = input("Vali kas kivi, paber või käärid: ")
+#     võimalused = ["kivi", "paber", "käärid"]
+#     arvuti_valik = random.choice(võimalused)
+
+#     print("Sinu valik: "+mängija_valik)
+#     print("Arvuti valik: "+arvuti_valik)
+    
+
+#     if mängija_valik == arvuti_valik:
+#         print("Tegid arvutiga sama valiku. Te jäite viiki!")
+#         print("Seis on",mäs,":",ars)
+
+#     elif mängija_valik == "kivi":
+#         if arvuti_valik == "käärid":
+#             mäs += 1
+#             print("Kivi lõhub käärid - sinu võit!")
+#             print("Seis on",mäs,":",ars)
+#         else: 
+#             ars += 1
+#             print("Paber katab kivi - arvuti võit!")
+#             print("Seis on",mäs,":",ars)
             
 
         
-    elif mängija_valik == "paber":
-        if arvuti_valik == "kivi":
-            mäs += 1
-            print("Paber katab kivi - sinu võit!")
-            print("Seis on",mäs,":",ars)
-        else:
-            ars += 1
-            print("Käärid lõikavad paberi - arvuti võit!")
-            print("Seis on",mäs,":",ars)
+#     elif mängija_valik == "paber":
+#         if arvuti_valik == "kivi":
+#             mäs += 1
+#             print("Paber katab kivi - sinu võit!")
+#             print("Seis on",mäs,":",ars)
+#         else:
+#             ars += 1
+#             print("Käärid lõikavad paberi - arvuti võit!")
+#             print("Seis on",mäs,":",ars)
             
             
-    elif mängija_valik == "käärid":
-        if arvuti_valik == "paber":
-            mäs += 1
-            print("Käärid lõikavad paberi - sinu võit!")
-            print("Seis on",mäs,":",ars)
-        else:
-            ars += 1
-            print("Kivi lõhub käärid - arvuti võit!")
-            print("Seis on",mäs,":",ars)
+#     elif mängija_valik == "käärid":
+#         if arvuti_valik == "paber":
+#             mäs += 1
+#             print("Käärid lõikavad paberi - sinu võit!")
+#             print("Seis on",mäs,":",ars)
+#         else:
+#             ars += 1
+#             print("Kivi lõhub käärid - arvuti võit!")
+#             print("Seis on",mäs,":",ars)
+
+
+    
+
 
             
-    #mängi_uuesti = input("Kas soovid veel mängida?: (jah/ei): ")
-   # if mängi_uuesti != "jah":
-      #  break
+   
         
+
